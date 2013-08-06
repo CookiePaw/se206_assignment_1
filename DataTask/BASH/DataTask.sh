8e8e06e152e2b94f06eac40edaa6de1375597d5a
Bert Huang <dendeer82@gmail.com>
2013-08-06 22:37:11 +1200

    Refined DataTask.sh, made it much short and simpler

--- DataTask/BASH/DataTask.sh
+++ DataTask/BASH/DataTask.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-CURDIR='.'
 OUTPUT='out.txt'
-
-if [ -e $OUTPUT ]; then > $OUTPUT; fi
-
-for FILE in *; do
-	if [[ $FILE == *.contact ]]; then
-		CONTENT="$CONTENT $(<$FILE)"
-	fi
-done;
+CONTENT=`cat *.contact`
 echo $CONTENT | xargs -n1 | sort -u > $OUTPUT
-
