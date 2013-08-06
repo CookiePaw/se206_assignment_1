b869c39b35d43a17966ebd5ea87fa073b67912ee
Bert Huang <dendeer82@gmail.com>
2013-08-06 23:03:39 +1200

    Added comment to DataTask.sh and FileTask.sh

--- DataTask/BASH/DataTask.sh
+++ DataTask/BASH/DataTask.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Set output file name as 'out.txt'
 OUTPUT='out.txt'
+
+# Read and concatinate the contents of file
 CONTENT=`cat *.contact`
+
+# The variable CONTENT could either be in vertical or horizontal manner.
+# Using 'xarg -n1' will convert it to 1 column of text, then the 
+# 'sort -u' will take care of the uniqueness and the 'alphabetical' 
+# order. The result will be written into 'out.txt'.
 echo $CONTENT | xargs -n1 | sort -u > $OUTPUT
--- FileTask/BASH/FileTask.sh
+++ FileTask/BASH/FileTask.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+# If command line is empty, read from STDIN
 if [ $# == 0 ]; then
 	read -p "Directory: " DIRECTORY
 	read -p "Contact Name: " NAME
 	read -p "Contact Number: " NUMBER
+
+# Otherwise, read from command line
 else
 	DIRECTORY=$1
 	NAME=$2
 	NUMBER=$3
 fi
 
+# If directory does not exist, create.
 if [ ! -d $DIRECTORY ]; then mkdir -p $DIRECTORY; fi
 
+# Initialise variables accordingly
 EXTENSION=".contact"
 FILENAME=$NAME$EXTENSION
+
+# If file already exists, create a new file with +1 index
 COUNT=0
 while [ -e $DIRECTORY'/'$FILENAME ]; do
 	COUNT=$((COUNT + 1))
 	FILENAME=$NAME'('$COUNT')'$EXTENSION
 done
 
+# Write contact number to the created file.
 echo $NUMBER > $DIRECTORY'/'$FILENAME
 
